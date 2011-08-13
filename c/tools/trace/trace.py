b5fb5bafd29ff10b73da6431054cf5a3d18456de
xihuafeng <huafengxi@gmail.com>
2011-08-13 21:46:17 +0800

    c/tools/trace/trace.py can handle huge amount of func calls

--- c/tools/trace/trace.py
+++ c/tools/trace/trace.py
@@ -6,52 +6,58 @@ TRACE=trace.png ./trace.py ./a.out
 TRACE=trace.plain ./trace.py ./a.out
 '''
 trace_so_source = r"""
-#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
-static FILE *g_trace_output;
-void trace_init()__attribute__((no_instrument_function, constructor));
-void trace_destroy() __attribute__((no_instrument_function, destructor));
+#define MAX_N_TRACE_ITEM (1<<20)
 
-#define trace_output(...) if(g_trace_output)fprintf(g_trace_output, __VA_ARGS__)
-#define error(...)  { fprintf(stderr, __VA_ARGS__); perror(NULL); }
-void trace_init()
+void panic(const char* msg)
 {
-        char exe[1024];
-        int count;
-        if(!(g_trace_output = fopen(getenv("TRACE"), "w"))){
-                error("fopen('%s')=>NULL: ", getenv("TRACE"));
-                return;
-        }
-        if((count = readlink("/proc/self/exe", exe, sizeof(exe))) < 0)
-                return;
-        exe[count] = 0;
-        trace_output("%s\n", exe);
+        perror(msg);
+        exit(-1);
 }
 
-void trace_destroy()
+void** g_trace_items;
+void** g_trace_items_current;
+int g_trace_fd;
+__attribute__((no_instrument_function, constructor)) void trace_init()
 {
-        if(g_trace_output)
-                fclose(g_trace_output);
+        long buf_len = MAX_N_TRACE_ITEM * sizeof(void*);
+        if((g_trace_fd = open(getenv("TRACE"), O_RDWR|O_CREAT, S_IRWXU)) < 0) panic("open");
+        if(ftruncate(g_trace_fd, buf_len) < 0) panic("ftruncate");
+        if(!(g_trace_items = mmap(0, buf_len, PROT_READ|PROT_WRITE, MAP_SHARED, g_trace_fd, 0)))
+                panic("mmap");
+        g_trace_items_current = g_trace_items;
+}
+
+__attribute__((no_instrument_function, destructor)) void trace_destroy()
+{
+        long buf_len = MAX_N_TRACE_ITEM * sizeof(void*);
+        munmap(g_trace_items, buf_len);
+        ftruncate(g_trace_fd, sizeof(void*) * (g_trace_items_current - g_trace_items));
+        close(g_trace_fd);
 }
 
 void __cyg_profile_func_enter( void *this, void *callsite)
 {
-        trace_output("E %p\n", this);
+        (g_trace_items_current < g_trace_items + MAX_N_TRACE_ITEM -1) && (*g_trace_items_current++ = this);
 }
 
 void __cyg_profile_func_exit( void *this, void *callsite)
 {
-        trace_output("X %p\n", this);
+        (g_trace_items_current < g_trace_items + MAX_N_TRACE_ITEM -1) && (*g_trace_items_current++ = ~0L);
 }
 """
 
 import sys
 import re
 import os, os.path
+import signal
 import math
 from subprocess import call, Popen, PIPE, STDOUT
 from collections import Counter
+import array
 
 def info(msg):
     sys.stderr.write(msg)
@@ -92,34 +98,33 @@ def accmulate(func, seq, initial):
 def popen(cmd, input=None):
     return Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate(input=input)[0]
 
-def addr2line(exe, addrs):
-    return popen('addr2line -f -C -e %s'%(exe), '\n'.join(addrs))
-
 def last_n_part_of_path(path, n):
     return '/'.join(path.split('/')[-n:])
 
-def common_prefix(ls, maxdepth=2):
+def remove_common_prefix(ls, maxdepth=2):
     prefix = re.sub('/[^/]*$', '/', os.path.commonprefix(ls))
     return [last_n_part_of_path(i[len(prefix):], maxdepth) for i in ls]
 
-def addr_translate(trace, exe, maxdepth=2):
-    enter_exit, addrs = zip(*trace)
+def addr2name(addrs, exe, maxdepth=2):
+    def addr2line(exe, addrs):
+        return popen('addr2line -f -C -e %s'%(exe), '\n'.join('0x%x'%i for i in addrs))
     fn_trace = re.findall('^(.+)\n(.+)$', addr2line(exe, addrs), re.M)
     funcs, lineno = zip(*fn_trace)
-    return zip(enter_exit, funcs, common_prefix(lineno, maxdepth))
+    return dict(zip(addrs, zip(funcs, remove_common_prefix(lineno, maxdepth))))
 
 def call_chains(trace):
     def call_link((_enter_exit, parent), (enter_exit, func, lineno)):
         return enter_exit, enter_exit == 'E' and (parent, (func, lineno)) or parent[0]
     return [chain for enter_exit, chain in accmulate(call_link, trace, (None, (None,None))) if enter_exit == 'E']
 
-def plain(trace, exe, maxdepth=2):
-    return Counter((parent, func) for ((_parent,parent), func) in call_chains(addr_translate(trace, exe, maxdepth)))
+def resolve_counter_names(counters, exe, maxdepth=2):
+    name_map = addr2name([callee for (caller, callee) in counters], exe, maxdepth)
+    return dict(((name_map[caller], name_map[callee]), count) for (caller, callee), count in counters.items() if caller)
 
-def trace2plain(trace, exe, maxdepth=2):
+def trace2plain(counters, exe, maxdepth=2):
     def func_repr(func):
         return func and '@'.join(func)
-    return '\n'.join('%d %s => %s'%(count, func_repr(caller), func_repr(callee)) for (caller, callee), count in plain(trace, exe, maxdepth).items())
+    return '\n'.join('%d %s => %s'%(count, func_repr(caller), func_repr(callee)) for (caller, callee), count in resolve_counter_names(counters, exe, maxdepth).items())
 
 def trace2dot(trace, exe, maxdepth=2):
     def func_repr(func):
@@ -138,7 +143,7 @@ node[shape=box, style=filled];
 rankdir=LR;
 %s
 };'''
-    return digraph% '\n'.join(call_link2dot(caller, callee, count) for (caller, callee),count in plain(trace, exe, maxdepth).items())
+    return digraph% '\n'.join(call_link2dot(caller, callee, count) for (caller, callee),count in resolve_counter_names(trace, exe, maxdepth).items())
 
 def create_trace_so(dir):
     write('%s/trace.c'%(dir), trace_so_source)
@@ -147,27 +152,51 @@ def create_trace_so(dir):
 def traced_call(trace_file, trace_so, args):
     os.putenv('LD_PRELOAD', '%s/trace.so'%(cwd))
     os.putenv('TRACE', trace_file)
-    call(args)
+    p = Popen(args)
     os.putenv('LD_PRELOAD', '')
+    return p
 
-def trace_term_plain(trace_content, exefile, outfile):
-    write(outfile, trace2plain(trace_content, exefile))
+def get_trace_counters(file):
+    stack, counter = array.array('L', [0]), Counter()
+    with open(file) as f:
+        try:
+            while f:
+                stack.fromfile(f, 1)
+                if stack[-1] == 0:
+                    break
+                if stack[-1] == 0xFFFFFFFFFFFFFFFF:
+                    stack.pop()
+                    stack.pop()
+                else:
+                    counter.update([(stack[-2], stack[-1])])
+        except EOFError:
+            pass
+    return counter
 
-def trace_term_dot(trace_content, exefile, outfile):
-    write(outfile, trace2dot(trace_content, exefile))
+def trace_term_plain(counters, exefile, outfile):
+    write(outfile, trace2plain(counters, exefile))
+
+def trace_term_dot(counters, exefile, outfile):
+    write(outfile, trace2dot(counters, exefile))
     
-def trace_term_png(trace_content, exefile, outfile):
-    print popen('dot -Tpng -o %s'% outfile, input=trace2dot(trace_content, exefile))
-    
+def trace_term_png(counters, exefile, outfile):
+    print popen('dot -Tpng -o %s'% outfile, input=trace2dot(counters, exefile))
+
+def create_trace_file(trace_file, trace_so, args):
+    p = traced_call('%s.trace'% trace_basename, '%s/trace.so'% cwd, sys.argv[1:])
+    signal.signal(signal.SIGINT, lambda signo, frame: os.kill(p.pid, signal.SIGINT))
+    p.wait()
+    return os.path.exists(trace_file)
+
 if __name__ == '__main__':
-    len(sys.argv) > 1 or err(__doc__)
-    trace_output = os.getenv('TRACE') or os.path.basename(sys.argv[1]) + '.png'
+    signal.signal(signal.SIGINT, lambda signo, frame: err('caught SIGINT.\n'))
+    trace_output = os.getenv('TRACE') or (len(sys.argv) > 1 and os.path.basename(sys.argv[1]) + '.png') or err(__doc__)
     trace_basename, trace_ext = os.path.splitext(trace_output)
     trace_term = trace_ext and trace_ext[1:]
     cwd = os.path.dirname(os.path.realpath(__file__))
     os.path.exists('%s/trace.so'%(cwd)) or create_trace_so(cwd) or err('create "%s/trace.so" failed.\n'%(cwd))
-    traced_call('%s.trace'% trace_basename, '%s/trace.so'% cwd, sys.argv[1:])
-    with open('%s.trace'%(trace_basename)) as f:
-        exefile, trace_content = f.readline().strip(), [i.split() for i in f.readlines()]
-    _term = globals().get('trace_term_%s'%(trace_term)) or (lambda trace_content, exefile, outfile: err('no term defined for %s'% trace_term))
-    _term(trace_content, exefile, trace_output)
+    sys.argv[1:] and (create_trace_file('%s.trace'% trace_basename, '%s/trace.so'% cwd, sys.argv[1:]) or err('create "%s.trace" failed.\n'% trace_basename))
+    exefile = sys.argv[1]
+    counters = get_trace_counters('%s.trace'% trace_basename) or err('not valid "%s.trace".\n'% trace_basename)
+    _term = globals().get('trace_term_%s'%(trace_term)) or (lambda counters, exefile, outfile: err('no term defined for %s.'% trace_term))
+    _term(counters, exefile, trace_output)
